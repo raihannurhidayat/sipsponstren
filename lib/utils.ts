9ce365366c55835b3ad71d5f8f4d5245a313984c
raihannurhidayat <grandrx041@gmail.com>
2025-04-24 18:59:49 +0700

    feat: add view dashboard user

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getInitials(name: string) {
+  // Split the name into words by space
+  const words = name.split(' ');
+
+  // Get the first letter of the first word and the first letter of the second word
+  const initials = words[0][0] + words[1][0];
+
+  // Return the initials in uppercase
+  return initials.toUpperCase();
+}
